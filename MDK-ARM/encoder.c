96c9487dc6d3ac4566b2fe5659bd3338ad062539
xfp23 <xfp57064@gmail.com>
2025-01-05 00:23:49 +0800

    格式化

--- MDK-ARM/encoder.c
+++ MDK-ARM/encoder.c
@@ -5,50 +5,47 @@
 #ifndef ON
 #define ON (1)
 #endif
-	
+
 #ifndef OFF
 #define OFF (0)
 #endif
 
-
 void encoder_Init(volatile Encoder_Class_t *encoder)
 {
-	memset((void *)encoder,0,sizeof(Encoder_Class_t)); // 初始化编码器
+	memset((void *)encoder, 0, sizeof(Encoder_Class_t)); // 初始化编码器
 }
 
 /**
-* @brief 在中断中回调此函数
-*/
+ * @brief 在中断中回调此函数
+ */
 void encoderA_Callback(volatile Encoder_Class_t *encoder)
 {
 	static uint8_t count = 0;
 	count++;
-	if(encoder->flag.EN_B == OFF)
+	if (encoder->flag.EN_B == OFF)
 	{
 		encoder->flag.EN_A = ON;
-		
-		
-		if(count % 2 == 0&& encoder->value++ >= ENCODER_MAX)
+
+		if (count % 2 == 0 && encoder->value++ >= ENCODER_MAX)
 		{
 			count = 0;
 			encoder->value = ENCODER_MAX;
 		}
 	}
-	
 }
 
 /**
-* @brief 在中断中回调此函数
-*/
+ * @brief 在中断中回调此函数
+ */
 void encoderB_Callback(volatile Encoder_Class_t *encoder)
 {
 	static uint8_t count = 0;
 	count++;
-	if(encoder->flag.EN_A == OFF)
+	if (encoder->flag.EN_A == OFF)
 	{
 		encoder->flag.EN_B = ON;
-		
-		if(count % 2 == 0 && encoder->value-- <= ENCODER_MAIN)
+
+		if (count % 2 == 0 && encoder->value-- <= ENCODER_MAIN)
 		{
 			count = 0;
 			encoder->value = ENCODER_MAIN;
@@ -56,16 +53,15 @@ void encoderB_Callback(volatile Encoder_Class_t *encoder)
 	}
 }
 /**
-*@brief 在主循环中调用此函数
-*/
+ *@brief 在主循环中调用此函数
+ */
 void encoderLoop(volatile Encoder_Class_t *encoder)
 {
-	if(HAL_GPIO_ReadPin(ENCODER_A_PORT,ENCODER_A_PIN) == RESET 
-	   == HAL_GPIO_ReadPin(ENCODER_B_PORT,ENCODER_B_PIN) == RESET)
+	if (HAL_GPIO_ReadPin(ENCODER_A_PORT, ENCODER_A_PIN) == RESET == HAL_GPIO_ReadPin(ENCODER_B_PORT, ENCODER_B_PIN) == RESET)
 	{
-//		HAL_Delay(20);
-//		if(HAL_GPIO_ReadPin(ENCODER_A_PORT,ENCODER_A_PIN) == RESET 
-//			&& HAL_GPIO_ReadPin(ENCODER_B_PORT,ENCODER_B_PIN) == RESET)
-				memset((void*)&encoder->flag,0,sizeof(encoder_Flag_t));
+		//		HAL_Delay(20);
+		//		if(HAL_GPIO_ReadPin(ENCODER_A_PORT,ENCODER_A_PIN) == RESET
+		//			&& HAL_GPIO_ReadPin(ENCODER_B_PORT,ENCODER_B_PIN) == RESET)
+		memset((void *)&encoder->flag, 0, sizeof(encoder_Flag_t));
 	}
 }
